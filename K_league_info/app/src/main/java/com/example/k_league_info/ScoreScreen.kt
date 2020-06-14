6e6611128bd158547728e8f1a4775bc2e88e731a
dawit95 <dawit0310@gmail.com>
2020-06-14 21:56:26 +0900

    ScoreDetail 기본 골자 뷰

---
+++ K_league_info/app/src/main/java/com/example/k_league_info/ScoreScreen.kt
@@ -0,0 +1,11 @@
+package com.example.k_league_info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score_screen)
+    }
+}

ff58c717da80eacf998ab165a68c8188ff6f85dc
OzawaLio <bvon95@naver.com>
2020-06-15 13:06:27 +0900

    manifest added

--- K_league/app/src/main/java/com/example/k_league/teamScreen.kt
+++ K_league/app/src/main/java/com/example/k_league/teamScreen.kt
@@ -1,4 +1,20 @@
 package com.example.k_league
 
-class teamScreen {
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class teamScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.team_screen)
+
+        ImageView gangwon = new 
+        imageView.setOnClickListener(new View.onClickListener(){
+
+        })
+    }
 }

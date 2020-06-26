cc67b14eb0cb51caa097bed3f31d242df6e2fe73
dawit95 <dawit0310@gmail.com>
2020-06-27 00:22:30 +0900

    recyclerView 설정

---
+++ K_league_info/app/src/main/java/com/example/k_league_info/Score_item.kt
@@ -0,0 +1,9 @@
+package com.example.k_league_info
+
+// 일단 표시해야하는 목록을 가진 item
+class Score_item (
+    val position: String, val backNumber: String, val player: String,
+            val replacement: String, val goal: String, val assistance: String,
+            val shooting: String, val foul:String, val yellowCard: String,
+            val redCard: String, val cornerKick: String, val offside: String ) {
+}

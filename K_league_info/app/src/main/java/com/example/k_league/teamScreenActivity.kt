f98ca4540309d123175fe17b23ada374d0e94ce1
OzawaLio <bvon95@naver.com>
2020-06-29 14:20:23 +0900

    recycler view adding

--- K_league_info/app/src/main/java/com/example/k_league/teamScreenActivity.kt
+++ K_league_info/app/src/main/java/com/example/k_league/teamScreenActivity.kt
@@ -1,16 +1,34 @@
 package com.example.k_league
 
+import android.content.AbstractThreadedSyncAdapter
 import android.content.Intent
 import android.media.Image
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class teamScreenActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.team_screen)
+
+        viewManager = GridLayoutManager(this)
+        viewAdapter = MyAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.teamscreen_recycler_view).apply{
+            setHasFixedSize(true)
+
+            layoutManager = viewManager
+
+            adapter = viewAdapter
+        }
 
         val selected_button = findViewById<ImageButton>(R.id.gangwon)
         selected_button.setOnClickListener {
